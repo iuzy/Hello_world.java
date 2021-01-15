f2767caf90aaf71d7e9e53e3d129f4026e0654ea
Ilze <gaile.ilze@inbox.lv>
2021-01-15 21:06:49 +0200

    Ou Yeah

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -5,8 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("Hello in JAVA world!");
 	//FIXME
-    //todo
+
         System.out.println("TGIF");
+        //todo
 	    //Some irrelevant comment
 
 
