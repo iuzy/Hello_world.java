a86ec0218d95669cf4e0c5bb29b437c9c0b765cc
Ilze <gaile.ilze@inbox.lv>
2021-01-15 20:47:32 +0200

    Initial commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	System.out.println("Hello in JAVA world!");
+	System.out.println("FIXME is quite useful feature");
+	//FIXME
+    //todo
+
+
+
+    }
+}
